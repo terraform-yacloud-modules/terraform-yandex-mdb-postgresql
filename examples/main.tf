81a35dc49f60956cd845b4fdce931dc4fdd88b22
Anton Patsev <patsev.anton@gmail.com>
2024-07-31 12:38:29 +0600

    Создание модуля postgresql

---
+++ examples/main.tf
@@ -0,0 +1,75 @@
+data "yandex_client_config" "client" {}
+
+module "network" {
+  source = "git::https://github.com/terraform-yacloud-modules/terraform-yandex-vpc.git?ref=v1.0.0"
+
+  folder_id = data.yandex_client_config.client.folder_id
+
+  blank_name = "vpc-nat-gateway"
+  labels = {
+    repo = "terraform-yacloud-modules/terraform-yandex-vpc"
+  }
+
+  azs = ["ru-central1-a", "ru-central1-b", "ru-central1-d"]
+
+  private_subnets = [["10.4.0.0/24"], ["10.5.0.0/24"], ["10.6.0.0/24"]]
+
+  create_vpc         = true
+  create_nat_gateway = true
+}
+
+########
+module "postgresql_cluster" {
+  source = "../"
+
+  name        = "my-postgresql-cluster"
+  environment = "PRESTABLE"
+  network_id  = module.network.vpc_id
+  description = "My PostgreSQL cluster description"
+  folder_id   = data.yandex_client_config.client.folder_id
+
+  config = {
+    version = "15"
+    resources = {
+      resource_preset_id = "s2.micro"
+      disk_type_id       = "network-ssd"
+      disk_size          = 16
+    }
+    postgresql_config = {
+      max_connections                   = 395
+      enable_parallel_hash              = true
+      autovacuum_vacuum_scale_factor    = 0.34
+      default_transaction_isolation     = "TRANSACTION_ISOLATION_READ_COMMITTED"
+      shared_preload_libraries          = "SHARED_PRELOAD_LIBRARIES_AUTO_EXPLAIN,SHARED_PRELOAD_LIBRARIES_PG_HINT_PLAN"
+    }
+  }
+
+  hosts = [
+    {
+      zone             = "ru-central1-a"
+      subnet_id        = module.network.private_subnets_ids[0]
+      assign_public_ip = true
+      name             = "host-a"
+    },
+    {
+      zone             = "ru-central1-b"
+      subnet_id        = module.network.private_subnets_ids[1]
+      assign_public_ip = true
+      name             = "host-b"
+    },
+    {
+      zone             = "ru-central1-d"
+      subnet_id        = module.network.private_subnets_ids[2]
+      assign_public_ip = true
+      name             = "host-d"
+    },
+  ]
+
+  maintenance_window = {
+    type = "WEEKLY"
+    day  = "SAT"
+    hour = 12
+  }
+
+  depends_on = [module.network]
+}
--- main.tf
+++ main.tf
@@ -1,1 +1,38 @@
-locals {}
+resource "yandex_mdb_postgresql_cluster" "postgresql_cluster" {
+  name        = var.name
+  environment = var.environment
+  network_id  = var.network_id
+  description = var.description
+  folder_id   = var.folder_id
+  labels      = var.labels
+  security_group_ids = var.security_group_ids
+  deletion_protection = var.deletion_protection
+
+  config {
+    version = var.config.version
+    resources {
+      resource_preset_id = var.config.resources.resource_preset_id
+      disk_type_id       = var.config.resources.disk_type_id
+      disk_size          = var.config.resources.disk_size
+    }
+    postgresql_config = var.config.postgresql_config
+  }
+
+  dynamic "host" {
+    for_each = var.hosts
+    content {
+      zone             = host.value.zone
+      subnet_id        = host.value.subnet_id
+      assign_public_ip = host.value.assign_public_ip
+      name             = host.value.name
+      priority         = host.value.priority
+      replication_source_name = host.value.replication_source_name
+    }
+  }
+
+  maintenance_window {
+    type = var.maintenance_window.type
+    day  = var.maintenance_window.day
+    hour = var.maintenance_window.hour
+  }
+}
