eb7d5e57de89f6250ef7c684ab5fb12e90eb5553
Anton Patsev <patsev.anton@gmail.com>
2024-08-04 09:10:40 +0600

    Поправил форматирование

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 resource "yandex_mdb_postgresql_cluster" "postgresql_cluster" {
-  name        = var.name
-  environment = var.environment
-  network_id  = var.network_id
-  description = var.description
-  folder_id   = var.folder_id
-  labels      = var.labels
-  security_group_ids = var.security_group_ids
+  name                = var.name
+  environment         = var.environment
+  network_id          = var.network_id
+  description         = var.description
+  folder_id           = var.folder_id
+  labels              = var.labels
+  security_group_ids  = var.security_group_ids
   deletion_protection = var.deletion_protection
 
   config {
@@ -21,11 +21,11 @@ resource "yandex_mdb_postgresql_cluster" "postgresql_cluster" {
   dynamic "host" {
     for_each = var.hosts
     content {
-      zone             = host.value.zone
-      subnet_id        = host.value.subnet_id
-      assign_public_ip = host.value.assign_public_ip
-      name             = host.value.name
-      priority         = host.value.priority
+      zone                    = host.value.zone
+      subnet_id               = host.value.subnet_id
+      assign_public_ip        = host.value.assign_public_ip
+      name                    = host.value.name
+      priority                = host.value.priority
       replication_source_name = host.value.replication_source_name
     }
   }
