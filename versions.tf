ccb586120daff57de61a25d41ab905d338a00a06
Anton <patsev.anton@gmail.com>
2025-10-11 19:54:56 +0600

    Исправлены ошибки pre-commit и обновлена документация

--- versions.tf
+++ versions.tf
@@ -1,5 +1,9 @@
 terraform {
   required_providers {
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 3.0.0"
+    }
     yandex = {
       source  = "yandex-cloud/yandex"
       version = ">= 0.72.0"
